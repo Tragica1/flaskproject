d882563fbf26dd423a958bf3241832b6202f6fa8
Maxim Odinokov <odi.max2002@gmail.com>
2022-05-10 23:21:06 +0300

    add models.py

---
+++ models.py
@@ -0,0 +1,33 @@
+from config import db, manager
+from flask_login import UserMixin
+
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=False)
+    photoLink = db.Column(db.String(500), nullable=True)
+    password = db.Column(db.String(30), nullable=False)
+    favorites = db.Column(db.PickleType, nullable=True)
+    added = db.Column(db.PickleType, nullable=True)
+
+    def __repr__(self):
+        return f'users {self.id}, {self.name}'
+
+
+@manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
+
+class Recipes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    photoLink = db.Column(db.String(500), nullable=False)
+    title = db.Column(db.String(300), nullable=False)
+    recipe = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'recipes {self.id}, {self.name}'
+
+
