3a54383de8e7a845fe362e7e504b9350ff7d8745
Maxim Odinokov <odi.max2002@gmail.com>
2022-05-31 00:47:54 +0300

    last commit

--- tmp.py
+++
@@ -1,8 +0,0 @@
-from config import db
-from models import Users
-from werkzeug.security import generate_password_hash
-
-db.create_all()
-user = Users(name='Maxim', email='maxim@mail.ru', password=generate_password_hash('user'))
-db.session.add(user)
-db.session.commit()
