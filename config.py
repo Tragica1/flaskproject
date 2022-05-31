720219156292607d27be34222b86d59e5a207a7a
Maxim Odinokov <odi.max2002@gmail.com>
2022-05-31 14:59:19 +0300

    all

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-upl_folder = 'static\\images'
+upl_folder = 'static/images'
 app.config['SECRET_KEY'] = 'damazhor'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kuxny.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
